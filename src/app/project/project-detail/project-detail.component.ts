b6795b019f70cad7192097d499702e299ef45f82
Shemar Lindie <shemarlindie@gmail.com>
2021-09-11 01:50:43 -0400

    make dialog scrollable

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -3,13 +3,11 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {Project} from "../project";
 import {ProjectService} from "../project.service";
-import {fadeAnimation} from "../../app.animation";
 
 @Component({
   selector: 'project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.sass'],
-  animations: [fadeAnimation],
 })
 export class ProjectDetailComponent implements OnInit {
   project: Project
